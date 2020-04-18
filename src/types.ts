783be5c944c03e482233f9d1437f7d363aaec4ec
Grant Holle <hollegrant@gmail.com>
2020-04-18 08:35:35 +0800

    Find type tinkering

--- src/types.ts
+++ src/types.ts
@@ -11,6 +11,11 @@ export interface Endpoint {
   readonly name?: string
 }
 
+export interface Request {
+  id: string|number
+  language?: string
+}
+
 export interface EndpointNamespace {
   readonly prefix: string
   readonly endpoints: Array<Endpoint>
@@ -26,8 +31,62 @@ export interface LimitOptions {
   reset: number
 }
 
+export interface Response {}
+
 export interface AuthenticationToken {
   success?: boolean
   expires_at?: string
   request_token?: string
+}
+
+export interface RequestOptions {
+  appendToResponse?: string
+}
+
+export interface RequestParams {
+  id?: string|number
+}
+
+export interface MovieResult {
+  poster_path?: string
+  adult?: boolean
+  overview?: string
+  release_date?: string
+  genre_ids?: Array<number>
+  id?: number
+  media_type: 'movie'
+  original_title?: string
+  original_language?: string
+  title?: string
+  backdrop_path?: string
+  popularity?: number
+  vote_count?: number
+  video?: boolean
+  vote_average?: number
+}
+
+export interface TvResult {
+  poster_path?: string
+  popularity?: number
+  id?: number
+  overview?: string
+  backdrop_path?: string
+  vote_average?: number
+  media_type: 'tv'
+  first_air_date?: string
+  origin_country?: Array<string>
+  genre_ids?: Array<number>
+  original_language?: string
+  vote_count?: number
+  name?: string
+  original_name?: string
+}
+
+export interface PersonResult {
+  profile_path?: string
+  adult?: boolean
+  id?: number
+  name?: string
+  popularity?: number
+  known_for?: Array<MovieResult|TvResult>
 }
