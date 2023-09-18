08bf0cf9c940a6c9e3ebdd20babb70cbb7e3c43c
chadacious <chad@medlor.com>
2023-09-18 08:53:59 -0700

    fix iso_639_1 backdrop datatype

--- src/request-types.ts
+++ src/request-types.ts
@@ -119,7 +119,7 @@ export interface Profile {
   aspect_ratio?: number
   file_path?: string
   height?: number
-  iso_639_1?: null
+  iso_639_1?: string
   vote_average?: number
   vote_count?: number
   width?: number
