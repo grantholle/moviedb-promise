731d37e3c1623ce90617789a85c1641ff67654ad
Grant Holle <hollegrant@gmail.com>
2023-09-19 08:14:32 +0400

    Build and publish

--- dist/request-types.d.ts
+++ dist/request-types.d.ts
@@ -58,6 +58,10 @@ export interface PersonResult {
     popularity?: number;
     known_for?: Array<MovieResult | TvResult>;
 }
+export interface EpisodeResult extends SimpleEpisode {
+    media_type: 'tv_episode';
+    runtime?: string;
+}
 export interface Person {
     birthday?: string | null;
     known_for_department?: string;
@@ -156,15 +160,15 @@ export interface SimpleEpisode {
     episode_number?: number;
     id?: number;
     name?: string;
+    order?: number;
     overview?: string;
     production_code?: string;
+    rating?: number;
     season_number?: number;
     show_id?: number;
     still_path?: string;
     vote_average?: number;
     vote_count?: number;
-    rating?: number;
-    order?: number;
 }
 export interface Network {
     name?: string;
@@ -336,7 +340,7 @@ export interface FindResponse extends Response {
     movie_results: Array<MovieResult>;
     tv_results: Array<TvResult>;
     person_results: Array<PersonResult>;
-    tv_episode_results: Array<object>;
+    tv_episode_results: Array<EpisodeResult>;
     tv_season_results: Array<object>;
 }
 export interface SearchRequest extends RequestParams {
